4bad61567e7cdd07afc36cbecdf9f7f9b77fa10e
Dung Bean <s3642407@student.rmit.edu.au>
2019-01-22 20:56:55 +1100

    update connection error echo

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,3 +1,7 @@
 <?php
   $con = mysqli_connect("localhost","root","","ecommerce");
+
+  if (mysqli_connect_errno()) {
+    echo "Fail to connect to mysql: ".mysqli_connect_error();
+  }
 ?>
--- functions/functions.php
+++ functions/functions.php
@@ -2,6 +2,10 @@
 
   $con = mysqli_connect("localhost","root","","ecommerce");
 
+  if (mysqli_connect_errno()) {
+    echo "Fail to connect to mysql: ".mysqli_connect_error();
+  }
+
   //Getting the categories
   function getCats() {
     global $con;
