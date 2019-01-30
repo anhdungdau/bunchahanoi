c1575035756d4bba520b3e520ad8cdf2ed1ab901
Dung Bean <s3642407@student.rmit.edu.au>
2019-01-30 19:19:43 +1100

    paypal online payment integration

--- payment.php
+++ payment.php
@@ -30,7 +30,7 @@
 
 			$total +=$values;
 
-    }
+}
 }
 
 			// getting Quantity of the product
@@ -45,7 +45,6 @@
 			if($qty==0){
 
 			$qty=1;
-
 			}
 			else {
 
@@ -54,36 +53,34 @@
 			}
 
 
-      ?>
-    <h2 align="center" style="padding:2px;">Pay now with Paypal:</h2>
+?>
+<h2 align="center" style="padding:2px;">Pay now with Paypal:</h2>
 
-		<p style="text-align:center;"><img src="paypal.jpg" width="200" height="100" /></p>
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" >
 
-    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" >
+<!-- Identify your business so that you can collect the payments. -->
+<input type="hidden" name="business" value="sriniv_1293527277_biz@inbox.com">
 
-    <!-- Identify your business so that you can collect the payments. -->
-    <input type="hidden" name="business" value="sriniv_1293527277_biz@inbox.com">
+<!-- Specify a Buy Now button. -->
+<input type="hidden" name="cmd" value="_xclick">
 
-    <!-- Specify a Buy Now button. -->
-    <input type="hidden" name="cmd" value="_xclick">
+<!-- Specify details about the item that buyers will purchase. -->
+<input type="hidden" name="item_name" value="<?php echo $product_name; ?>">
+<input type="hidden" name="item_number" value="<?php echo $pro_id; ?>">
+<input type="hidden" name="amount" value="<?php echo $total; ?>">
+<input type="hidden" name="quantity" value="<?php echo $qty; ?>">
+<input type="hidden" name="currency_code" value="USD">
 
-    <!-- Specify details about the item that buyers will purchase. -->
-    <input type="hidden" name="item_name" value="<?php echo $product_name; ?>">
-    <input type="hidden" name="item_number" value="<?php echo $pro_id; ?>">
-    <input type="hidden" name="amount" value="<?php echo $total; ?>">
-    <input type="hidden" name="quantity" value="<?php echo $qty; ?>">
-    <input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="return" value="http://www.onlinetuting.com/myshop/paypal_success.php"/>
+<input type="hidden" name="cancel_return" value="http://www.onlinetuting.com/myshop/paypal_cancel.php"/>
 
-    <input type="hidden" name="return" value="http://www.onlinetuting.com/myshop/paypal_success.php"/>
-    <input type="hidden" name="cancel_return" value="http://www.onlinetuting.com/myshop/paypal_cancel.php"/>
+<!-- Display the payment button. -->
+<input type="image" name="submit" border="0"
+src="paypal_button.png"
+alt="PayPal - The safer, easier way to pay online">
+<img alt="" border="0" width="1" height="1"
+src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
 
-    <!-- Display the payment button. -->
-    <input type="image" name="submit" border="0"
-    src="paypal_button.png"
-    alt="PayPal - The safer, easier way to pay online">
-    <img alt="" border="0" width="1" height="1"
-    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+</form>
 
-    </form>
-
-    </div>
+</div>
