32a2ca68c40bcaa897c7867760ef52ac29817061
Dung Bean <s3642407@student.rmit.edu.au>
2019-02-06 21:32:35 +1100

    insert cat

--- admin_area/insert_cat.php
+++ admin_area/insert_cat.php
@@ -2,24 +2,24 @@
 <form action="" method="post" style="padding:80px;">
 
 <b>Insert New Category:</b>
-<input type="text" name="new_cat" required/> 
-<input type="submit" name="add_cat" value="Add Category" /> 
+<input type="text" name="new_cat" required/>
+<input type="submit" name="add_cat" value="Add Category" />
 
 </form>
 
-<?php 
-include("includes/db.php"); 
+<?php
+include("includes/db.php");
 
 	if(isset($_POST['add_cat'])){
-	
-	$new_cat = $_POST['new_cat'];
-	
-	$insert_cat = "insert into categories (cat_title) values ('$new_cat')";
 
-	$run_cat = mysqli_query($con, $insert_cat); 
-	
+	$new_cat = $_POST['new_cat'];
+
+	$insert_cat = "INSERT into categories (cat_title) values ('$new_cat')";
+
+	$run_cat = mysqli_query($con, $insert_cat);
+
 	if($run_cat){
-	
+
 	echo "<script>alert('New Category has been inserted!')</script>";
 	echo "<script>window.open('index.php?view_cats','_self')</script>";
 	}
